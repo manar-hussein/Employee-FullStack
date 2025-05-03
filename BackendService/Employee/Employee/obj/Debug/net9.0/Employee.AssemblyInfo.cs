f1d12ff234f68727b7868de52198b7f5549c700d
Manar-Nabil <manar.nabil@business-expert.ai>
2025-05-03 19:04:58 +0300

    update

--- BackendService/Employee/Employee/obj/Debug/net9.0/Employee.AssemblyInfo.cs
+++ BackendService/Employee/Employee/obj/Debug/net9.0/Employee.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e79566c1c3f3570f90d7f629a7ae501ae76998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18a907f4cf46814225a0fb66076146382049ef48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FrontEnd/EmployeeManagmenet/src/environments/environment.ts
+++ FrontEnd/EmployeeManagmenet/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7106/api',
+  apiUrl: 'http://localhost:5220/api',
   apiVersion: 'v1',
   apiTimeout: 30000,
   enableDebug: true
